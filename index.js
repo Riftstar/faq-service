72c31b0df41aac600d946a99eacbff68487fca52
Riftstar <swift.raid@gmail.com>
2020-03-11 13:34:49 +0000

    Add cors and refine code

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const port = 8080;
 const service = express();
+const cors = require('cors');
+
+service.use(cors());
 
 const faq = require('./faqs');
 
 service.get('/faqs/', (req, res) => {
     if (req.query.q) {
-        let word = req.query.q
-        console.log(word);
-        let patt = new RegExp(word, 'i');
+        let word = new RegExp(req.query.q, 'i');
         let filter = faq.filter(function (found) {
-            return found.question.match(patt);
+            return found.question.match(word);
         });
         return res.json(filter);
     }
