3bfa22fee511b6704de73bdc12566e70439f197e
Riftstar <swift.raid@gmail.com>
2020-03-11 12:12:42 +0000

    Made search case insensitive

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const faq = require('./faqs');
 service.get('/faqs/', (req, res) => {
     if (req.query.q) {
         let word = req.query.q
+        console.log(word);
+        let patt = new RegExp(word, 'i');
         let filter = faq.filter(function (found) {
-            return found.question.includes(word);
+            return found.question.match(patt);
         });
         return res.json(filter);
     }
